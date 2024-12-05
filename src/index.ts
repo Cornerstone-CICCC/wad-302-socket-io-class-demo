67b2f9cc462ddcf5eb1e2dc1e063013298255f58
aishtiaq <awshafishtiaque@gmail.com>
2024-12-04 20:18:00 -0800

    messages being updated in the ui

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,22 @@ const io = new Server(server);
 io.on("connection", (socket) => {
   console.log("A user connected", socket.id);
 
+  socket.on("joinRoom", (room, num1) => {
+    console.log(`==> User ${socket.id} joined room ${room}`);
+    socket.join(room);
+  });
+
+  socket.on('leaveRoom', room =>{
+    console.log(`==> User ${socket.id} LEFT room ${room}`);
+    socket.leave(room)
+  })
+
+  socket.on("message", ({room, message}) => {
+    console.log("received on server:", message);
+    
+    io.to(room).emit('message', {id: socket.id, message})
+  });
+
   socket.on("disconnect", () => {
     console.log("A user disconnected", socket.id);
   });
